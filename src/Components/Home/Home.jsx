27215b9110d8624c0ee4a244c427fb78c0d01073
Ziyadk147 <Ziyadkhan2333@gmail.com>
2024-10-05 19:57:15 +0500

    timebox completed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,16 @@
 import Navbar from "../NavBar/Navbar.jsx";
+import Timebox from "../Timebox/Timebox.jsx";
 
 export default function Home(){
 
     return (
-        <main className={"bg-gradient-to-l from-[#000000] via-[#444444] to-[#444444] h-full flex flex-row justify-center "}>
-            <Navbar></Navbar>
-        </main>
+        <div className={"bg-gradient-to-l from-[#000000] via-[#444444] to-[#444444] h-full flex flex-row justify-center  "}>
+            <Navbar/>
+            <main className={" my-52 mx-72 flex justify-start w-full"}>
+                <Timebox/>
+            </main>
+
+        </div>
     )
 
 }
---
+++ src/Components/Timebox/Timebox.jsx
@@ -0,0 +1,15 @@
+export default function Timebox(){
+    return  (
+        <div className={"flex flex-col rounded-3xl shadow-[rgba(0,0,15,0.5)_10px_5px_4px_0px] py-2 px-52 bg-lighter-dark  justify-center items-center h-72 w-96"}>
+            <h1 className=" flex flex-row text-stone-100 text-4xl font-bold">
+                Athens
+            </h1>
+            <h2 className={"flex flex-row  text-stone-100 font-bold text-8xl"} >
+                09:20
+            </h2>
+            <p className={"text-stone-100 flex flex-row "}>
+                Thursday
+            </p>
+        </div>
+    )
+}
